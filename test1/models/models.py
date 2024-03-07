87f9d6d46a9ea3ab911130e411790c8423ba84b9
elsaddik <prog.ahmedabdelsadek>
2024-03-07 11:08:00 +0200

    this commit

---
+++ test1/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class test1(models.Model):
+#     _name = 'test1.test1'
+#     _description = 'test1.test1'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
