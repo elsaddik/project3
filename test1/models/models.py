cb4a832d28eb2a66c6be88fbe0eaa9b0c07dc80d
elsaddik <prog.ahmedabdelsadek>
2024-03-07 11:12:38 +0200

    eee

--- test1/models/models.py
+++ test1/models/models.py
@@ -12,7 +12,7 @@ class test1(models.Model):
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
